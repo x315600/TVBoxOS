c349e55717e5648eca002ce546eec8d103f3df27
315600 <512687325@qq.com>
2022-09-21 21:31:42 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -272,7 +272,7 @@ public class HomeActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 if (!useCacheConfig)
-                                    Toast.makeText(HomeActivity.this, "jar加载成功", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(HomeActivity.this, "加载成功", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         }, 50);
